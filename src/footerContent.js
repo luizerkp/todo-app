92087e285fb4bbd85d6b73bc0222a2cd24e0f4f0
Luis Tamarez <luistamarez@hotmail.com>
2022-07-28 16:43:30 -0700

    Add initial webpack set up and layout     * Add .gitignore file     * Add normalize.css     * Add intial css file     * Add initial file structure

---
+++ src/footerContent.js
@@ -0,0 +1,31 @@
+import githubLogo from './imgs/GitHubMarkSmallLight.png';
+
+
+// adds footer content to the page
+var footer = (function() {
+    const footerDiv = document.createElement('div');
+    footerDiv.classList.add('footer');
+    const footerPara_1 = document.createElement('p');
+    const footerPara_2 = document.createElement('p');
+    const a = document.createElement('a');
+    a.href = "https://github.com/luizerkp";
+    const myGithubLogo = document.createElement('img');
+    myGithubLogo.src= githubLogo
+    a.appendChild(myGithubLogo)
+    a.setAttribute('id', 'github-logo');
+    a.target = '_blank';
+    const date = new Date().getFullYear();
+    footerPara_1.textContent = `Copyright © ${date} Luis Tamarez`
+    footerPara_2.textContent = "All Rights Reserved";
+    footerDiv.appendChild(footerPara_1);
+    footerDiv.appendChild(a);
+    footerDiv.appendChild(footerPara_2);
+
+    const buildFooter = () => document.body.appendChild(footerDiv);
+    
+    return {
+        buildFooter: buildFooter
+    }
+})();
+
+export { footer };
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './css/style.css';
+import './css/normalize.css';
+import { footer } from './footerContent.js';
+
+// var buildPageContent = (function (){
+
+// })();
+
+var buildFooterContent = (function () {
+    footer.buildFooter();
+})();
